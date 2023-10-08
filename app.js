438d6c23cb85edd6d5f51308593d29ea92ab9c26
Maxwell Rutherford <javert.hugo@yandex.ru>
2023-10-09 01:02:37 +0300

    add jsdoc

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const btn = document.querySelector('#myBtn')
 const spoiler = document.querySelector('#spoiler')
 
 let isSpoilerClosed = spoiler.matches('.closed')
+
+/**
+ * Переключает видимость элемента "спойлер" при нажатии на кнопку.
+ * @returns {void}
+ */
 const toggleVisibility = () => {
   if (isSpoilerClosed) {
     spoiler.classList.remove('closed')
